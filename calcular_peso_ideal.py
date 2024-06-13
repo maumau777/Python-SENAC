4480c6546674a72b91c251a528c92c7a3d05d235
Mauricio <mrsfilho07@gmail.com>
2024-06-13 20:03:36 -0300

    adiucionado

--- calcular_peso_ideal.py
+++ calcular_peso_ideal.py
@@ -14,3 +14,23 @@ def calcular_peso_ideal(altura, sexo):
 peso_ideal = calcular_peso_ideal(altura, sexo)
 if peso_ideal is not None:
     print("Seu peso ideal é:", peso_ideal, "quilos.")
+
+# calculo do imc (m/h²)
+
+peso = float(input("Digite seu peso (em quilos): "))
+
+imc = peso/(altura ** 2)
+print("Seu IMC é:", imc,".")
+
+if imc < 18.5:
+    print("Abaixo do peso")
+elif 18.5 <= imc < 25:
+    print("Peso normal")
+elif 25 <= imc < 30:
+    print("Sobrepeso")
+elif 30 <= imc < 35:
+    print("Obesidade grau 1")
+elif 35 <= imc < 40:
+    print("Obesidade grau 2")
+else:
+    print("Obesidade grau 3")
